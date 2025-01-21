1880a1ae9cc4c2b5fb8940eb36aba19a7bcb6dd2
Angel Montes <angelmontes963@gmail.com>
2025-01-21 16:33:35 -0600

    Creacion de componente Footer y Navegacion

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './componentes/login/login';
 import Home from './componentes/home/Home';
+import Navegacion from './componentes/navegacion/Navegacion';
+import Footer from './componentes/footer/Footer';
 
 function App() {
     return (
         <Router>
+            <Navegacion />
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
+            <Footer />
         </Router>
     );
 }
---
+++ src/componentes/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Footer = () => {
+
+    return (
+        <div class="container">
+            <footer class="py-3 my-4">
+                <p class="text-center text-body-secondary">© 2024 Angel Montes</p>
+            </footer>
+        </div>
+    );
+
+}
+
+
+export default Footer;
