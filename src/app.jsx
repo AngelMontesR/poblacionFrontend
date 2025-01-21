e06050441ff6f9ec62b51f2e959fececf65c2a6a
Angel Montes <angelmontes963@gmail.com>
2025-01-20 22:31:14 -0600

    Creacion de rutas y de pagina Home inicializamos

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './componentes/login/login';
+import Home from './componentes/home/Home';
 
 function App() {
     return (
-        <div>
-            <Login />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </Router>
     );
 }
 
