4c1ff71593e10108c02fb5532e3c7cf6a57da865
Angel Montes <angelmontes963@gmail.com>
2025-01-20 22:52:06 -0600

    Se inicializa Home y Menu

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
 const Home = () => {
-    return <h1>home</h1>;
+
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colSpan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
