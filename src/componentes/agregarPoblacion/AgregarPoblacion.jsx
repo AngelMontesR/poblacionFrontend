255d98f4132630dab219e9df2d129adf0cb41773
Angel Montes <angelmontes963@gmail.com>
2025-01-21 21:02:09 -0600

    Creacion de btn regresar en componente agregarPoblacion

--- src/componentes/agregarPoblacion/AgregarPoblacion.jsx
+++ src/componentes/agregarPoblacion/AgregarPoblacion.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function AgregarPoblacion() {
+    const navegar = useNavigate();
+
+    const home = () => {
+        navegar('/home');
+    }
 
     return (
         <div className="container mt-5">
-
+            <div className='mb-3'>
+                <button type="button" className="btn btn-secondary" onClick={home}>Regresar</button>
+            </div>
         </div>
     );
 };
